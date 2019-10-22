6941d33ee7c7446bf22bc8f73ecb166f04c80c44
liuyong <bjliuyong@jd.com>
2019-10-22 11:42:19 +0800

    修改成依赖netty http 数据结构 ，简化我们自己开发httpserver的流程

--- src/main/java/org/lemon/http/server/Task.java
+++ src/main/java/org/lemon/http/server/Task.java
@@ -1,16 +1,30 @@
 package org.lemon.http.server;
 
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.DefaultHttpRequest;
+import io.netty.handler.codec.http.HttpMessage;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.util.internal.AppendableCharSequence;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 import java.net.SocketException;
 
+import static io.netty.handler.codec.http.HttpConstants.*;
+
 public class Task implements Runnable {
 
     protected Socket socket;
 
-    public Task(Socket socket){
+    private String name;
+    private String value;
+    private long contentLength = Long.MIN_VALUE;
+    private HttpMessage message;
+
+    public Task(Socket socket) {
         this.socket = socket;
     }
 
@@ -19,16 +33,18 @@ public class Task implements Runnable {
         try {
             final byte[] MesssageBody = "Hello World!".getBytes("utf-8");
             while (true) {
-                HttpRequestMessage httpRequestMessage = parseRequestMessage(socket);
-                if(httpRequestMessage == null){
+                parseRequestMessage(socket);
+                if (message == null) {
                     break;
                 }
                 sendResponse(socket, MesssageBody);
-                //System.out.println("request line:" + httpRequestMessage.getRequestLine());
+                message = null;
             }
         } catch (SocketException se) {
             se.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
@@ -39,14 +55,13 @@ public class Task implements Runnable {
         }
     }
 
-
     /**
      * 将数据写入socket 流中
      *
      * @param socket
      * @param body
      */
-    private static void sendResponse(Socket socket, byte[] body) throws IOException {
+    private void sendResponse(Socket socket, byte[] body) throws IOException {
         final byte[] StatusLine = "HTTP/1.1 200 OK\r\n".getBytes("utf-8");
         final byte[] CRLF = "\r\n".getBytes("utf-8");
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -63,49 +78,152 @@ public class Task implements Runnable {
         outputStream.writeTo(socket.getOutputStream());
     }
 
-    private static HttpRequestMessage parseRequestMessage(Socket socket) throws IOException {
-        HttpRequestMessage requestMessage = new HttpRequestMessage();
+    private void parseRequestMessage(Socket socket) throws Exception {
         InputStream inputStream = socket.getInputStream();
 
         //readline
-        byte[] bytes = new byte[8*1024];
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+        byte[] bytes = new byte[8 * 1024];
+        AppendableCharSequence line = new AppendableCharSequence(1024);
         int step = 1;
         int n = 0;
-        boolean end = false;
         do {
             n = inputStream.read(bytes);
-
             if (n == -1) {
-                //no_data
-                //System.out.println("close!!");
-                return null;
+                return;
             }
             for (int i = 0; i < n; i++) {
-                if (bytes[i] == '\r') {
+                if (bytes[i] == CR) {
                     continue;
-                } else if (bytes[i] == '\n') {
-                    byte[] data = byteArrayOutputStream.toByteArray();
-                    if (data.length == 0) {
-                        end = true;
+                } else if (bytes[i] == LF) {
+
+
+                    if (line.length() == 0) {
+                        //解析完header
+                        long contentLength = contentLength();
+                        if(contentLength <= 0){
+                            step = 4;
+                        }
                         break;
                     }
-                    String line = new String(data);
-                    byteArrayOutputStream.reset();
+
                     if (step == 1) {
-                        requestMessage.setRequestLine(line);
+                        String[] initialLine = splitInitialLine(line);
+                        message = createMessage(initialLine);
                         step = 2;
+                        line.reset();
                     } else if (step == 2) {
-                        requestMessage.getHeaders().add(line);
+                        if(line.length() == 0){
+                            step = 3;
+                        }
+                        splitHeader(line);
+                        message.headers().add(name,value);
                         //解析请求头
+                        line.reset();
                     }
 
                     continue;
                 }
-                byteArrayOutputStream.write(bytes[i]);
+                line.append((char)bytes[i]);
+            }
+
+        }
+        while (step != 4);
+
+    }
+
+    private long contentLength() {
+        if (contentLength == Long.MIN_VALUE) {
+            contentLength = HttpUtil.getContentLength(message, -1L);
+        }
+        return contentLength;
+    }
+
+    private HttpMessage createMessage(String[] initialLine) throws Exception{
+        return new DefaultHttpRequest(
+            HttpVersion.valueOf(initialLine[2]),
+            HttpMethod.valueOf(initialLine[0]), initialLine[1], false);
+    }
+
+    private static String[] splitInitialLine(AppendableCharSequence sb) {
+        int aStart;
+        int aEnd;
+        int bStart;
+        int bEnd;
+        int cStart;
+        int cEnd;
+
+        aStart = findNonWhitespace(sb, 0);
+        aEnd = findWhitespace(sb, aStart);
+
+        bStart = findNonWhitespace(sb, aEnd);
+        bEnd = findWhitespace(sb, bStart);
+
+        cStart = findNonWhitespace(sb, bEnd);
+        cEnd = findEndOfString(sb);
+
+        return new String[] {
+            sb.subStringUnsafe(aStart, aEnd),
+            sb.subStringUnsafe(bStart, bEnd),
+            cStart < cEnd? sb.subStringUnsafe(cStart, cEnd) : "" };
+    }
+
+    private void splitHeader(AppendableCharSequence sb) {
+        final int length = sb.length();
+        int nameStart;
+        int nameEnd;
+        int colonEnd;
+        int valueStart;
+        int valueEnd;
+
+        nameStart = findNonWhitespace(sb, 0);
+        for (nameEnd = nameStart; nameEnd < length; nameEnd ++) {
+            char ch = sb.charAt(nameEnd);
+            if (ch == ':' || Character.isWhitespace(ch)) {
+                break;
             }
         }
-        while (!end);
-        return requestMessage;
+
+        for (colonEnd = nameEnd; colonEnd < length; colonEnd ++) {
+            if (sb.charAt(colonEnd) == ':') {
+                colonEnd ++;
+                break;
+            }
+        }
+
+        name = sb.subStringUnsafe(nameStart, nameEnd);
+        valueStart = findNonWhitespace(sb, colonEnd);
+        if (valueStart == length) {
+            value = "";
+        } else {
+            valueEnd = findEndOfString(sb);
+            value = sb.subStringUnsafe(valueStart, valueEnd);
+        }
+    }
+
+    private static int findNonWhitespace(AppendableCharSequence sb, int offset) {
+        for (int result = offset; result < sb.length(); ++result) {
+            if (!Character.isWhitespace(sb.charAtUnsafe(result))) {
+                return result;
+            }
+        }
+        return sb.length();
+    }
+
+    private static int findWhitespace(AppendableCharSequence sb, int offset) {
+        for (int result = offset; result < sb.length(); ++result) {
+            if (Character.isWhitespace(sb.charAtUnsafe(result))) {
+                return result;
+            }
+        }
+        return sb.length();
+    }
+
+    private static int findEndOfString(AppendableCharSequence sb) {
+        for (int result = sb.length() - 1; result > 0; --result) {
+            if (!Character.isWhitespace(sb.charAtUnsafe(result))) {
+                return result + 1;
+            }
+        }
+        return 0;
     }
 }
